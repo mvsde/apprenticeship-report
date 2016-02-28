732b69b94f535185ef6bfc9d16884def7a109217
Fynn Becker <info@fynn-becker.de>
2016-02-28 13:28:10 +0100

    Shift around some stuff, Implement config saving

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 // Node Modules
-const express = require('express');
+const fs         = require('fs');
+const express    = require('express');
 const bodyParser = require('body-parser');
-const open = require('open');
+const open       = require('open');
+
+// Settings
+const paths = {
+  config: './db/config.json',
+  database: './db/database.json'
+}
 
 // Load Configuration File
-var config   = require('./db/config.json');
+var config   = require(paths.config);
 
 // Load Database
-var database = require('./db/database.json');
+var database = require(paths.database);
 
 // Load Express App
 var app = express();
@@ -16,14 +23,25 @@ var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname));
 
-app.post('/save-settings', function(req, res) {
-  res.send('The next page...');
-  console.log(req.body);
+app.post('/save-config', function(req, res) {
+  res.send('Config gespeichert.');
+  console.log('\nConfig submitted.');
+
+  for (var key in req.body) {
+    if (req.body[key]) {
+      config[key] = req.body[key];
+    }
+  }
+
+  fs.writeFile(paths.config, JSON.stringify(config, null, 2), function(err) {
+    if (err) { return console.log(err); }
+    console.log('Config saved.');
+  });
 });
 
 
-app.listen(1337, function() {
-  console.log('Server started at http://127.0.0.1:1337');
-  console.log('Opening default web browser...');
-  open('http://127.0.0.1:1337/app.html');
+app.listen(0, function() {
+  console.log('Server started at http://127.0.0.1:' + this.address().port);
+  console.log('\nOpening default web browser...');
+  open('http://127.0.0.1:' + this.address().port);
 });
