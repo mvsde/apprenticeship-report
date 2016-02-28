216e5381cb3eada6ff63ff3092408373b65fba3f
Fynn Becker <info@fynn-becker.de>
2016-02-28 12:08:59 +0100

    Node Express is really great.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Node Modules
 const express = require('express');
 const bodyParser = require('body-parser');
+const open = require('open');
 
 // Load Configuration File
 var config   = require('./db/config.json');
@@ -10,3 +11,19 @@ var database = require('./db/database.json');
 
 // Load Express App
 var app = express();
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname));
+
+app.post('/save-settings', function(req, res) {
+  res.send('The next page...');
+  console.log(req.body.title);
+});
+
+
+app.listen(1337, function() {
+  console.log('Server started at http://127.0.0.1:1337');
+  console.log('Opening default web browser...');
+  open('http://127.0.0.1:1337');
+});
