14275ecc332b23a0868c0fae6e9357ac46bcba79
Fynn Becker <info@fynn-becker.de>
2016-02-29 07:36:10 +0100

    Change Express file serving mechanic for more flexibility

--- app.js
+++ app.js
@@ -31,113 +31,111 @@ app.use(express.static(__dirname + '/app'));
 
 
 
-// Handle HTML file delivery
-app.get('/:file', function(req, res) {
-  var options = {root: __dirname + '/app'};
+// Serve config page
+app.get('/config', function(req, res) {
+  fs.readFile('./app/config.html', 'utf8', function(error, data) {
+    jsdom.env(data, [], function(errors, window) {
+      for (var key in config) {
+        window.document.querySelector('[name="' + key + '"]').defaultValue = config[key];
+      }
 
-  switch (req.params.file) {
-    // Config
-    case "config":
-      fs.readFile('./app/config.html', 'utf8', function(error, data) {
-        jsdom.env(data, [], function(errors, window) {
-          for (var key in config) {
-            window.document.querySelector('[name="' + key + '"]').defaultValue = config[key];
-          }
+      res.send(window.document.documentElement.outerHTML);
+      window.close();
+    });
+  });
+});
 
-          res.send(window.document.documentElement.outerHTML);
-          window.close();
-        });
-      });
-      break;
 
-    // New entry
-    case "new":
-      fs.readFile('./app/new.html', 'utf8', function(error, data) {
-        jsdom.env(data, [], function(errors, window) {
-          res.send(window.document.documentElement.outerHTML);
-          window.close();
-        });
-      });
-      break;
 
-    // Edit entry
-    case "edit":
-      fs.readFile('./app/edit.html', 'utf8', function(error, data) {
-        jsdom.env(data, [], function(errors, window) {
-          res.send(window.document.documentElement.outerHTML);
-          window.close();
-        });
-      });
-      break;
 
-    // Print report
-    case "print":
-      fs.readFile('./app/print.html', 'utf8', function(error, data) {
-        jsdom.env(data, [], function(errors, window) {
-          for (var key in config) {
-            window.document.getElementById(key).innerHTML = config[key];
-          }
+// Serve new entry page
+app.get('/new', function(req, res) {
+  fs.readFile('./app/new.html', 'utf8', function(error, data) {
+    jsdom.env(data, [], function(errors, window) {
+      res.send(window.document.documentElement.outerHTML);
+      window.close();
+    });
+  });
+});
 
-          var entriesHTML = '';
-          const weekDays = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag'];
 
-          for (var i = 0, item; item = database.entries[i++];) {
-            var daysHTML = '';
-            var weekHours = 0;
 
-            for (var j = 0, day; day = item.work[j++];) {
-              var hours = day.hours.reduce(function(a, b) { return a + b; });
-              weekHours += hours;
 
-              daysHTML += '<tr>\
-                <td>' + weekDays[j - 1] + '</td>\
-                <td>' + day.tasks.join('<br>') + '</td>\
-                <td>' + day.hours.join('<br>') + '</td>\
-                <td>' + hours + '</td>\
-              </tr>';
-            }
+// Serve edit entry page
+app.get('/edit', function(req, res) {
+  fs.readFile('./app/edit.html', 'utf8', function(error, data) {
+    jsdom.env(data, [], function(errors, window) {
+      res.send(window.document.documentElement.outerHTML);
+      window.close();
+    });
+  });
+});
 
-            entriesHTML += '<section class="section section--entry page-break--none">\
-              <table class="table table--entry">\
-                <thead>\
-                  <tr>\
-                    <th colspan="2">' + item.start + ' bis ' + item.end + '</th>\
-                    <th colspan="2">Nr. ' + i + '</th>\
-                  </tr>\
-                  <tr>\
-                    <th>Tag</th>\
-                    <th>Ausgeführte Arbeiten oder Unterricht</th>\
-                    <th>Std.</th>\
-                    <th>Ges.</th>\
-                  </tr>\
-                </thead>\
-                <tfoot>\
-                  <tr>\
-                    <td colspan="3">Wochenstunden</td>\
-                    <td>' + weekHours + '</td>\
-                  </tr>\
-                </tfoot>\
-                <tbody>' +
-                  daysHTML +
-                '</tbody>\
-              </table>\
-              <p class="signature">Auszubildender</p>\
-              <p class="signature">Ausbilder/-in</p>\
-            </section>';
-          }
 
-          window.document.getElementById('entries').innerHTML = entriesHTML;
 
-          res.send(window.document.documentElement.outerHTML);
-          window.close();
-        });
-      });
-      break;
 
-    // Default page
-    default:
-      res.sendFile('/index.html', options);
-  }
+// Serve print page
+app.get('/print', function(req, res) {
+  fs.readFile('./app/print.html', 'utf8', function(error, data) {
+    jsdom.env(data, [], function(errors, window) {
+      for (var key in config) {
+        window.document.getElementById(key).innerHTML = config[key];
+      }
+
+      var entriesHTML = '';
+      const weekDays = ['Montag', 'Dienstag', 'Mittwoch', 'Donnerstag', 'Freitag'];
+
+      for (var i = 0, item; item = database.entries[i++];) {
+        var daysHTML = '';
+        var weekHours = 0;
+
+        for (var j = 0, day; day = item.work[j++];) {
+          var hours = day.hours.reduce(function(a, b) { return a + b; });
+          weekHours += hours;
+
+          daysHTML += '<tr>\
+            <td>' + weekDays[j - 1] + '</td>\
+            <td>' + day.tasks.join('<br>') + '</td>\
+            <td>' + day.hours.join('<br>') + '</td>\
+            <td>' + hours + '</td>\
+          </tr>';
+        }
+
+        entriesHTML += '<section class="section section--entry page-break--none">\
+          <table class="table table--entry">\
+            <thead>\
+              <tr>\
+                <th colspan="2">' + item.start + ' bis ' + item.end + '</th>\
+                <th colspan="2">Nr. ' + i + '</th>\
+              </tr>\
+              <tr>\
+                <th>Tag</th>\
+                <th>Ausgeführte Arbeiten oder Unterricht</th>\
+                <th>Std.</th>\
+                <th>Ges.</th>\
+              </tr>\
+            </thead>\
+            <tfoot>\
+              <tr>\
+                <td colspan="3">Wochenstunden</td>\
+                <td>' + weekHours + '</td>\
+              </tr>\
+            </tfoot>\
+            <tbody>' +
+              daysHTML +
+            '</tbody>\
+          </table>\
+          <p class="signature">Auszubildender</p>\
+          <p class="signature">Ausbilder/-in</p>\
+        </section>';
+      }
+
+      window.document.getElementById('entries').innerHTML = entriesHTML;
+
+      res.send(window.document.documentElement.outerHTML);
+      window.close();
+    });
+  });
 });
 
 
