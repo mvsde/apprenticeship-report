8544803c95433de49f102758052b3b06a03f9d36
Fynn Becker <info@fynn-becker.de>
2016-03-03 14:56:38 +0100

    Improve formWork JavaScript

--- app/js/main.js
+++ app/js/main.js
@@ -4,8 +4,8 @@
 
 ;(function() {
   // Cache DOM elements
-  const formGroup     = document.querySelector('.form-work');
-  const formGroupWork = document.querySelector('.form-work__input-group');
+  const formGroup     = document.querySelector('.form-work__group');
+  const formGroupWork = document.querySelector('.form-work__input');
 
   // Add event listener to + button
   document.getElementById('form-work__button').addEventListener('click', function(event) {
@@ -13,5 +13,9 @@
 
     // Append a clone of fromGroupWork to formGroup
     formGroup.appendChild(formGroupWork.cloneNode(true));
+
+    // Set focus to the newly created input field
+    var nameWork = document.querySelectorAll('[name="work"]');
+    nameWork[nameWork.length - 1].focus();
   });
 })();
